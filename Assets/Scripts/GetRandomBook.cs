30ffae0022eb7510a37a636cc4b0e2b681568ab5
samcaloiero <122567577+samcaloiero@users.noreply.github.com>
2024-04-18 17:38:45 -0400

    YES DEWEY RANDOM BOOK PRINTING AND TITLE GETTING WORKING

--- Assets/Scripts/GetRandomBook.cs
+++ Assets/Scripts/GetRandomBook.cs
@@ -1,16 +1,22 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace DefaultNamespace
 {
     public class GetRandomBook : MonoBehaviour
     {
         public Catalog catalog;
-
+        public TextMeshPro callNumText;
+        public TextMeshPro titleText;
+        public TextMeshPro authorText;
         public void UpdateText()
         {
             var book = catalog.GetRandomAvailableBook();
+
+            callNumText.text = book.callNumber;
+            titleText.text = book.title;
+            authorText.text = book.author;
             //text.asd = book.asdf
-            
         }
     }
 }
