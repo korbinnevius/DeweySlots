646f307171094723538f14f38e20fb05f5593949
Supererogato <terrariaprogamer2.0@gmail.com>
2024-04-20 19:57:28 -0400

    RefinedAnimations

---
+++ Assets/Scripts/TextboxEnable.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+
+public class TextboxEnable : MonoBehaviour
+{
+
+    public GameObject TextBox;
+    public float DelayTimer = 0f;
+
+    private void Start()
+    {
+        TextBox.SetActive(false);
+        Invoke("EnableTextBox", DelayTimer);
+    }
+
+    void EnableTextBox()
+    {
+        TextBox.SetActive(true);
+    }
+}
