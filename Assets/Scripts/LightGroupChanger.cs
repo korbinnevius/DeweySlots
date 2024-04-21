1b0da8c244e22b638cc7fb28c3f8d70969722860
Supererogato <terrariaprogamer2.0@gmail.com>
2024-04-21 12:39:13 -0400

    StlizedLights

---
+++ Assets/Scripts/LightGroupChanger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightGroupChanger : MonoBehaviour
+{
+
+    public GameObject AfterResults;
+    public GameObject BeforeResults;
+    public float DelayTimer = 0f;
+
+    private void Start()
+    {
+        AfterResults.SetActive(false);
+        BeforeResults.SetActive(true);
+        Invoke("EnableLightGroup", DelayTimer);
+        Invoke("DisableLightGroup", DelayTimer);
+    }
+
+    void EnableLightGroup()
+    {
+        AfterResults.SetActive(true);
+    } 
+    void DisableLightGroup()
+    {
+        BeforeResults.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/LightTimer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTimer : MonoBehaviour
+{
+
+    public GameObject LightGroup1;
+    public GameObject LightGroup2;
+    public float DelayTimer = 0f;
+
+    private void Start()
+    {
+        LightGroup1.SetActive(false);
+        LightGroup2.SetActive(true);
+        Invoke("EnableLights", DelayTimer);
+        Invoke("DisableLights", DelayTimer);
+    }
+
+    void EnableLights()
+    {
+        LightGroup1.SetActive(true);
+        Invoke("EnableLightsAgain", DelayTimer);
+    } 
+    void DisableLights()
+    {
+        LightGroup2.SetActive(false);
+        Invoke("DisableLightAgain", DelayTimer);
+    }
+
+    void EnableLightsAgain()
+    {
+        LightGroup1.SetActive(false);
+        Invoke("EnableLights", DelayTimer);
+    }
+    void DisableLightAgain()
+    {
+        LightGroup2.SetActive(true);
+        Invoke("DisableLights", DelayTimer);
+    }
+}
