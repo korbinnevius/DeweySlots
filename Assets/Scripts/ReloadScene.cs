ad0b4b0921eccca26c08968d8ca92a4a577060e7
Supererogato <terrariaprogamer2.0@gmail.com>
2024-04-21 13:09:45 -0400

    ReloadSceneScript

--- Assets/Scripts/ReloadScene.cs
+++ Assets/Scripts/ReloadScene.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ReloadScene : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ReloadGame();
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    public void ReloadGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
